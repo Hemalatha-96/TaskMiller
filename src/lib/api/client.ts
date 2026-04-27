ff76403a32c40ca68b25fb4872ab1ef511c8356a
Hemalatha-96 <hemalatha.p@orotron.com>
2026-04-27 17:01:12 +0530

    added all changes

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getAccessToken, getRefreshToken, saveTokens, clearTokens } from '../../utils/token'
 import { useAuthStore } from '../../store/auth.store'
 
-const BASE_URL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000'
+const BASE_URL = import.meta.env.VITE_PUBLIC_API_URL ?? 'http://localhost:3000'
 
 export const apiClient = axios.create({
   baseURL: BASE_URL,
